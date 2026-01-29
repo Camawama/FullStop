1e8eb20f08cb18062e8c8d46a48bddb58fb08d7e
Camawama <ackruger400@gmail.com>
2026-01-29 16:32:03 -0600

    Initial Architectury 1.20.1 multiplatform workspace

---
+++ common/src/main/java/net/camacraft/fullstop/ExampleMod.java
@@ -0,0 +1,9 @@
+package net.camacraft.fullstop;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "fullstop";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
